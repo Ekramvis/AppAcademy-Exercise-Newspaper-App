fc0a954d3d23d95e55da5238820515d85d39117d
Angelo and Scott <angelo+scott@appacademy.io>
2013-05-15 11:38:37 -0400

    login system works using user.save

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :password, :password_confirmation
 	has_secure_password
 
-	validates :name, :password, :password_confirmation, presence: true
+	validates :name, :password, :password_confirmation, presence: true, on: :create
 
 	has_many :subscriptions
 	has_many :subscription_plans, through: :subscriptions
